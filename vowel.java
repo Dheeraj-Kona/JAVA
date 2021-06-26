98c40514a926f725f3b4f7a539de037dccfe38dc
Dheeraj Kona <66221964+Dheeraj-Kona@users.noreply.github.com>
2021-06-26 20:03:32 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class vow
+{
+    public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+         char ch = in.next().charAt(0);
+         
+	if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+System.out.println("vowels");
+    }
+    else{
+        System.out.println("Consonants");
+    }
+	}
+}
